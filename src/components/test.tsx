799c02eb447d298fddb825de4db797b6bccf1875
emotek <emotek@pop-os.localdomain>
2024-04-02 17:35:21 -0300

    2.0.12

--- src/components/test.tsx
+++ src/components/test.tsx
@@ -1,25 +1,28 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { useRouter, redirect, usePathname } from "next/navigation";
 export default function TestComponent() {
   const router = useRouter();
   const pathName = usePathname();
+  const openBottomsheet = useCallback(() => {
+    router.push("www.twitter.com");
+  }, [router]);
   // window.location.replace("//www.google.com.br");
-  const openBottomsheet = (event: any) => {
-    event.preventDefault();
-    window.location.replace("//www.google.com.br");
-    if (pathName == "/test") {
-      router.back();
-    }
-  };
+  // const openBottomsheet = (event: any) => {
+  //   event.preventDefault();
+  //   window.location.replace("//www.google.com.br");
+  //   if (pathName == "/test") {
+  //     router.back();
+  //   }
+  // };
 
   console.log("pathNaem", pathName);
   useEffect(() => {
-    addEventListener("popstate", (event) => openBottomsheet(event));
+    addEventListener("popstate", (event) => openBottomsheet());
     return () => {
       removeEventListener("popstate", (event) => openBottomsheet);
     };
-  }, []);
+  }, [openBottomsheet]);
   return (
     <div className="w-full h-screen">
       <h1 className="text-2xl font-bold text-black mx-auto">
