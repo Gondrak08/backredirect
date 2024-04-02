ed116670b13acd4314e0d5e42365894cd573d358
emotek <emotek@pop-os.localdomain>
2024-04-02 14:51:59 -0300

    1.0.35

--- src/components/test.tsx
+++ src/components/test.tsx
@@ -5,19 +5,19 @@ export default function TestComponent() {
   const router = useRouter();
   const [isBack, setIsBack] = useState<boolean>(false);
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      window.addEventListener("click", function (event) {
-        event.preventDefault();
-        window.history.pushState({}, "", null);
-      });
-      window.addEventListener("popstate", function (event) {
-        event.preventDefault();
-        console.log("User clicked the browser buttons popstate");
-        setIsBack(true);
-        // window.history.pushState(null, "", "https://www.google.com.br");
-        // window.location.reload();
-      });
-    }
+    // if (typeof window !== "undefined") {
+    window.addEventListener("click", function (event) {
+      event.preventDefault();
+      window.history.pushState({}, "", null);
+    });
+    window.addEventListener("popstate", function (event) {
+      event.preventDefault();
+      console.log("User clicked the browser buttons popstate");
+      setIsBack(true);
+      // window.history.pushState(null, "", "https://www.google.com.br");
+      // window.location.reload();
+    });
+    // }
   }, []);
   useEffect(() => {
     console.log("quack", isBack);
