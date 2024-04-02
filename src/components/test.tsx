80534b937c166da2dd79e6dae4f98182ab71d196
emotek <emotek@pop-os.localdomain>
2024-04-02 16:47:40 -0300

    2.0.3

--- src/components/test.tsx
+++ src/components/test.tsx
@@ -4,10 +4,11 @@ import { useRouter, redirect } from "next/navigation";
 export default function TestComponent() {
   const openBottomsheet = (event: any) => {
     event.preventDefault();
+    window.location.hash = "#replace";
     console.log("quack");
-    window.location.replace("//www.google.com.br");
-    window.location.href = "//www.google.com.br";
-    window.location.reload();
+    // window.location.replace("//www.google.com.br");
+    // window.location.href = "//www.google.com.br";
+    // window.location.reload();
     // redirect("//www.google.com.br");
     // window.history.pushState(null, "", "//www.google.com.br");
     // window.history.back();
