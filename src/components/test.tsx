8d0f49972ff67c1dd48baa90711d7d88718d57b1
emotek <emotek@pop-os.localdomain>
2024-04-02 19:37:54 -0300

    1.0.4

--- src/components/test.tsx
+++ src/components/test.tsx
@@ -1,25 +1,10 @@
 "use client";
 import { useEffect, useState } from "react";
-import { useRouter, redirect, usePathname } from "next/navigation";
+// import { useRouter, redirect, usePathname } from "next/navigation";
 export default function TestComponent() {
-  const [isBack, setIsBack] = useState<boolean>(false);
-  const pathName = usePathname();
-  const router = useRouter();
   useEffect(() => {
-    if (typeof window != "undefined")
-      window.addEventListener("popstate", (event) => {
-        setIsBack(true);
-        console.log("hii");
-      });
+    console.log("window", window);
   });
-
-  useEffect(() => {
-    if (isBack) {
-      console.log("isBackChange!", isBack);
-
-      router.push("//www.google.com.br");
-    }
-  }, [isBack, router]);
   return (
     <div className="w-full h-screen">
       <h1 className="text-2xl font-bold text-black mx-auto">
