0646c63abd849a18131ee0806d6aa111eff7e3f7
emotek <emotek@pop-os.localdomain>
2024-04-02 14:11:39 -0300

    corno vei

--- src/components/test.tsx
+++ src/components/test.tsx
@@ -3,23 +3,31 @@ import { useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 export default function TestComponent() {
-  const router = useRouter();
-
-  useEffect(() => {
-    const handlePopstate = () => {
-      window.location.href = "https://www.google.com.br";
-      // window.location.reload();
-      router.push("https://www.google.com.br");
-      window.history.pushState(null, "", "https://www.google.com.br");
-      router.back();
-    };
-
-    window.addEventListener("popstate", handlePopstate);
-
-    return () => {
-      window.removeEventListener("popstate", handlePopstate);
-    };
+  window.addEventListener("click", function () {
+    window.history.pushState({}, "", null);
   });
+  window.addEventListener("popstate", function () {
+    console.log("User clicked the browser buttons popstate");
+    window.history.pushState(null, "", "https://www.google.com.br");
+    window.location.reload();
+  });
+  // const router = useRouter();
+  //
+  // useEffect(() => {
+  //   const handlePopstate = () => {
+  // window.location.href = "https://www.google.com.br";
+  // window.location.reload();
+  //     router.push("https://www.google.com.br");
+  // window.history.pushState(null, "", "https://www.google.com.br");
+  //     router.back();
+  //   };
+  //
+  //   window.addEventListener("popstate", handlePopstate);
+  //
+  //   return () => {
+  //     window.removeEventListener("popstate", handlePopstate);
+  //   };
+  // });
   return (
     <div className="w-full h-screen">
       <h1 className="text-2xl font-bold text-black mx-auto">
