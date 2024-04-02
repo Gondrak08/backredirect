465a247b52e2ef4dc2d45d3165199b1b56d4ded7
emotek <emotek@pop-os.localdomain>
2024-04-02 12:17:00 -0300

    1.0.15

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -4,18 +4,22 @@ import { useEffect } from "react";
 
 export default function Home() {
   useEffect(() => {
-    const handlePopstate = () => {
-      // Redireciona o usuário para fora do site
-      window.location.replace("https://www.google.com");
+    window.addEventListener("beforepopstate", (event) => {
+      if (window.location.pathname === "/test") {
+        event.preventDefault();
+        window.location.href = "https://www.google.com";
+      }
+    });
+
+    return () => {
+      window.removeEventListener("beforepopstate", (event) => {
+        if (window.location.pathname === "/test") {
+          event.preventDefault();
+          window.location.href = "https://www.google.com";
+        }
+      });
     };
-
-    // Adicione um listener para o evento 'popstate'
-    window.addEventListener("popstate", handlePopstate);
-
-    // Retorne a função de limpeza para remover o listener
-    return () => window.removeEventListener("popstate", handlePopstate);
   }, []);
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1 className="text-2xl text-black font-bold">RETORNE DESTA PÁGINA!</h1>
