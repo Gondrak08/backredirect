b2640f89d36b741881c61426a66945f7f2a3fd5b
emotek <emotek@pop-os.localdomain>
2024-04-02 14:29:02 -0300

    1.0.29

--- src/components/test.tsx
+++ src/components/test.tsx
@@ -1,48 +1,25 @@
 "use client";
 import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
 
 export default function TestComponent() {
   const [isBack, setIsBack] = useState<boolean>(false);
   useEffect(() => {
-    // if (typeof window !== "undefined") {
-    window.addEventListener("click", function () {
-      window.history.pushState({}, "", null);
-    });
-    window.addEventListener("popstate", function () {
-      console.log("User clicked the browser buttons popstate");
-      setIsBack(true);
-      // window.history.pushState(null, "", "https://www.google.com.br");
-      // window.location.reload();
-    });
-    // }
+    if (typeof window !== "undefined") {
+      window.addEventListener("click", function () {
+        window.history.pushState({}, "", null);
+      });
+      window.addEventListener("popstate", function () {
+        console.log("User clicked the browser buttons popstate");
+        setIsBack(true);
+        // window.history.pushState(null, "", "https://www.google.com.br");
+        // window.location.reload();
+      });
+    }
   }, []);
   useEffect(() => {
     console.log("quack", isBack);
   }, [isBack]);
-  // window.addEventListener("click", function () {
-  //   window.history.pushState({}, "", null);
-  // });
-  // window.addEventListener("popstate", function () {
-  //   console.log("User clicked the browser buttons popstate");
-  // });
-  // const router = useRouter();
-  //
-  // useEffect(() => {
-  //   const handlePopstate = () => {
-  // window.location.href = "https://www.google.com.br";
-  // window.location.reload();
-  //     router.push("https://www.google.com.br");
-  //     window.history.pushState(null, "", "https://www.google.com.br");
-  //     router.back();
-  //   };
-  //
-  //   window.addEventListener("popstate", handlePopstate);
-  //
-  //   return () => {
-  //     window.removeEventListener("popstate", handlePopstate);
-  //   };
-  // });
+
   return (
     <div className="w-full h-screen">
       <h1 className="text-2xl font-bold text-black mx-auto">
