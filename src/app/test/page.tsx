f31c3d5e8e81526520c688b0bbc8d09ab97871ff
emotek <emotek@pop-os.localdomain>
2024-04-02 11:58:39 -0300

    1.0.11

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,26 +1,31 @@
 "use client";
 import { useEffect } from "react";
 import { useRouter, usePathname } from "next/navigation";
+
+const isBrowser = () => typeof window !== "undefined";
+
 export default function Home() {
   const router = useRouter();
   const path = usePathname();
   // console.log("quack", path.)
-  useEffect(() => {
-    const handlePopState = () => {
-      // Verifique se o usuário está voltando da página inicial
-      // if ( === "/") {
-      // Redirecione o usuário para o link externo
-      router.push("https://www.google.com");
-      // }
-      console.log("hi");
-    };
-    if (typeof window !== "undefined")
-      // Adicione um ouvinte de evento para o evento "popstate"
-      window?.addEventListener("popstate", handlePopState);
-  }, [window]);
-  // Remova o ouvinte de evento quando o componente for desmontado
-  // return () => window.removeEventListener("popstate", handlePopState);
-  // });
+  // useEffect(() => {
+  const handlePopState = () => {
+    // Verifique se o usuário está voltando da página inicial
+    // if ( === "/") {
+    // Redirecione o usuário para o link externo
+    router.push("https://www.google.com");
+    // }
+    console.log("hi");
+  };
+
+  if (isBrowser()) {
+    window.addEventListener("popstate", handlePopState);
+    console.log("quack");
+    // }, []);
+    // Remova o ouvinte de evento quando o componente for desmontado
+    // return () => window.removeEventListener("popstate", handlePopState);
+    // });
+  }
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
