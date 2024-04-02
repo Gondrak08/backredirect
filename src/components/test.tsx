3a11e6ad864ed836a7313278065d27e51a922807
emotek <emotek@pop-os.localdomain>
2024-04-02 16:10:46 -0300

    1.0.46

--- src/components/test.tsx
+++ src/components/test.tsx
@@ -1,10 +1,12 @@
 "use client";
 import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
 
 export default function TestComponent() {
   const openBottomsheet = () => {
     window.location.replace("https://www.google.com.br");
+    window.location.href = "www.google.com.br";
+    window.location.reload();
+    // window.history.pushState(null, "", "https://www.google.com.br");
     //code to handle mounting the bottomsheet on DOM.
   };
 
