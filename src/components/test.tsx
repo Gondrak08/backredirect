cb20f2fa2d3c42e3e6013466ae7486b157fd3e47
emotek <emotek@pop-os.localdomain>
2024-04-02 16:56:01 -0300

    2.0.5

--- src/components/test.tsx
+++ src/components/test.tsx
@@ -2,10 +2,12 @@
 import { useEffect, useState } from "react";
 import { useRouter, redirect } from "next/navigation";
 export default function TestComponent() {
+  const router = useRouter();
+  // window.location.replace("//www.google.com.br");
   const openBottomsheet = (event: any) => {
-    event.preventDefault();
-    window.location.hash = "#replace";
+    // window.location.hash = "#replace";
     console.log("quack");
+    router.push("//www.google.com.br");
     // window.location.replace("//www.google.com.br");
     // window.location.href = "//www.google.com.br";
     // window.location.reload();
@@ -13,14 +15,16 @@ export default function TestComponent() {
     // window.history.pushState(null, "", "//www.google.com.br");
     // window.history.back();
     //code to handle mounting the bottomsheet on DOM.
+
+    event.preventDefault();
   };
 
   useEffect(() => {
     addEventListener("popstate", (event) => openBottomsheet(event));
     return () => {
-      removeEventListener("popstate", (event) => openBottomsheet(event));
+      removeEventListener("popstate", (event) => openBottomsheet);
     };
-  });
+  }, []);
   return (
     <div className="w-full h-screen">
       <h1 className="text-2xl font-bold text-black mx-auto">
