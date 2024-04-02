8a41d0bde1f8e55fb1cde47bf63ce7feb2f83bf7
emotek <emotek@pop-os.localdomain>
2024-04-02 15:05:48 -0300

    1.0.37:

--- src/components/test.tsx
+++ src/components/test.tsx
@@ -1,28 +1,37 @@
 "use client";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+
 export default function TestComponent() {
-  const router = useRouter();
   const [isBack, setIsBack] = useState<boolean>(false);
   useEffect(() => {
-    if (typeof window !== "undefined") {
-      window.addEventListener("click", function (event) {
-        event.preventDefault();
-        window.history.pushState({}, "", null);
-      });
-      window.addEventListener("popstate", function (event) {
-        event.preventDefault();
-        console.log("User clicked the browser buttons popstate");
-        setIsBack(true);
-        // window.history.pushState(null, "", "https://www.google.com.br");
-        // window.location.reload();
-      });
-    }
-  });
-  useEffect(() => {
-    console.log("quack", isBack);
-  }, [isBack]);
-
+    window.onpopstate = () => {
+      history.replaceState(null, "", "www.google.com.br");
+    };
+  }, []);
+  // window.addEventListener("click", function () {
+  //   window.history.pushState({}, "", null);
+  // });
+  // window.addEventListener("popstate", function () {
+  //   console.log("User clicked the browser buttons popstate");
+  // });
+  // const router = useRouter();
+  //
+  // useEffect(() => {
+  //   const handlePopstate = () => {
+  // window.location.href = "https://www.google.com.br";
+  // window.location.reload();
+  //     router.push("https://www.google.com.br");
+  //     window.history.pushState(null, "", "https://www.google.com.br");
+  //     router.back();
+  //   };
+  //
+  //   window.addEventListener("popstate", handlePopstate);
+  //
+  //   return () => {
+  //     window.removeEventListener("popstate", handlePopstate);
+  //   };
+  // });
   return (
     <div className="w-full h-screen">
       <h1 className="text-2xl font-bold text-black mx-auto">
